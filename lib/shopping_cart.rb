1ab16941c27566302f81208e5f097d8869cf6e40
Jack Puchalla <Jackpuchallalab@gmail.com>
2020-03-25 10:48:56 -0600

    iteration 3 complete

--- lib/shopping_cart.rb
+++ lib/shopping_cart.rb
@@ -14,5 +14,34 @@ class ShoppingCart
   def add_product(item)
     @products << item
   end
-  
+
+  def total_number_of_products
+     total_products = 0
+     @products.each {|product| total_products += product.quantity}
+     total_products
+  end
+
+  def is_full?
+    total = total_number_of_products
+    if total >= 30
+      true
+    else
+      false
+    end
+  end
+
+  def products_by_category(category)
+    product_with_category = []
+    @products.flat_map {|product| if product.category == category
+      product_with_category << product
+      end
+    }
+    product_with_category
+  end
+
+  def details
+   cart_details = {name: @name, capacity: @capacity}
+   cart_details
+  end
+
 end
--- test/shopping_cart_test.rb
+++ test/shopping_cart_test.rb
@@ -8,6 +8,8 @@ class ShoppingCartTest < Minitest::Test
   @cart = ShoppingCart.new("King Soopers", "30items")
   @product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
   @product2 = Product.new(:meat, 'chicken', 4.50, '2')
+  @product3 = Product.new(:paper, 'tissue paper', 1.25, '1')
+  @product4 = Product.new(:produce, 'apples', 0.99, '20')
   end
 
   def test_it_exists
@@ -26,6 +28,41 @@ class ShoppingCartTest < Minitest::Test
     assert_equal [@product1], @cart.add_product(@product1)
     assert_equal [@product1, @product2], @cart.add_product(@product2)
     assert_equal [@product1, @product2], @cart.products
+  end
+
+  def test_if_cart_can_measure_total_number_of_products
+    assert_equal [@product1], @cart.add_product(@product1)
+    assert_equal [@product1, @product2], @cart.add_product(@product2)
+    assert_equal [@product1, @product2, @product3], @cart.add_product(@product3)
+    assert_equal 13, @cart.total_number_of_products
+  end
+
+  def test_if_cart_is_full
+
+    assert_equal [@product1], @cart.add_product(@product1)
+    assert_equal [@product1, @product2], @cart.add_product(@product2)
+    assert_equal [@product1, @product2, @product3], @cart.add_product(@product3)
+    assert_equal false, @cart.is_full?
+    assert_equal [@product1, @product2, @product3, @product4], @cart.add_product(@product4)
+  end
+
+  def test_if_it_can_retrieve_products_by_category
+    assert_equal [@product1], @cart.add_product(@product1)
+    assert_equal [@product1, @product2], @cart.add_product(@product2)
+    assert_equal [@product1, @product2, @product3], @cart.add_product(@product3)
+    assert_equal [@product1, @product2, @product3, @product4], @cart.add_product(@product4)
+    assert_equal [@product1, @product3], @cart.products_by_category(:paper)
+
+  end
+
+  def test_if_it_can_retrieve_cart_details
+    assert_equal [@product1], @cart.add_product(@product1)
+    assert_equal [@product1, @product2], @cart.add_product(@product2)
+    assert_equal [@product1, @product2, @product3], @cart.add_product(@product3)
+    assert_equal [@product1, @product2, @product3, @product4], @cart.add_product(@product4)
+
+    assert_equal ({:name=>"King Soopers", :capacity=>"30items"}), @cart.details
+
 
   end
 
